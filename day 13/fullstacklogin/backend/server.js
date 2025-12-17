9c97ccce1d5ad3a62a332ac88996916a0c659288
sreee284 <srivarshini.u2024aids@sece.ac.in>
2025-12-17 09:04:17 +0530

    Add files via upload

---
+++ day 13/fullstacklogin/backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/smartpay")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
